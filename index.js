7697d398fa6c0152bac022f3d250bb3b9f8139f3
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-31 01:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(range) {
+          
   return function (start, end) {
-    l let dist = Match.abs(parseInt(start)-parseInt(end));
-      return dist > range ? false : true;
+    let dist = Match.abs(parseInt(start)-parseInt(end));
+    return dist > range ? false : true;
   }
 }
